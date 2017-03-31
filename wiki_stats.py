abf7d1f3fb1bc87d5b39c0b44fe52f89f4084786
Серафима Щербина < >
2017-03-31 22:24:50 +0300

    Added methods of class WikiGraphy

--- wiki_stats.py
+++ wiki_stats.py
@@ -8,10 +8,6 @@ import array
 
 import statistics
 
-from matplotlib import rc
-rc('font', family='Droid Sans', weight='normal', size=14)
-
-import matplotlib.pyplot as plt
 
 
 class WikiGraph:
@@ -20,43 +16,55 @@ class WikiGraph:
         print('Загружаю граф из файла: ' + filename)
 
         with open(filename) as f:
-            (n, _nlinks) = (0, 0) # TODO: прочитать из файла
-            
+            (n, _nlinks) = map(int,f.readline().split())  # TODO: прочитать из файла
             self._titles = []
-            self._sizes = array.array('L', [0]*n)
-            self._links = array.array('L', [0]*_nlinks)
-            self._redirect = array.array('B', [0]*n)
-            self._offset = array.array('L', [0]*(n+1))
+            self._sizes = array.array('L', [0] * n)
+            self._links = array.array('L', [0] * _nlinks)
+            self._redirect = array.array('B', [0] * n)
+            self._offset = array.array('L', [0] * (n + 1))
+            l = 0
+            for i in range(n):
+                title = f.readline()
+                self._titles.append(title)
+                size, redirect, amount_links = map(int, f.readline().split())
+                self._sizes[i] = size
+                self._redirect[i] = redirect
 
-            # TODO: прочитать граф из файла
+                self._offset[i] = l
+                for j in range(amount_links):
+                    self._links[j] = int(f.readline)
+                    l += 1
+            self._offset[n] = l
+
 
         print('Граф загружен')
 
     def get_number_of_links_from(self, _id):
-        pass
+        return self._offset[_id + 1] - self._offset[_id]
 
     def get_links_from(self, _id):
-        pass
+        return self._links[_offset[_id]:_offset[_id + 1]]
 
     def get_id(self, title):
-        pass
+        return len(self._titles)
 
     def get_number_of_pages(self):
-        pass
+        return self._n
 
     def is_redirect(self, _id):
-        pass
+        return self._redirect[_id]
 
     def get_title(self, _id):
-        pass
+        return self._titles[_id]
 
     def get_page_size(self, _id):
-        pass
+        return self._sizes[_id]
 
 
 def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
     plt.clf()
     # TODO: нарисовать гистограмму и сохранить в файл
+
 
 
 if __name__ == '__main__':
@@ -72,4 +80,7 @@ if __name__ == '__main__':
         print('Файл с графом не найден')
         sys.exit(-1)
 
-    # TODO: статистика и гистограммы
+
+    def bfs(g, start, finish):
+        dist=[-1 for i in range(len(g))]
+        pass
